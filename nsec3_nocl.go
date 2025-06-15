53e07e257b2e6ac8f111ee7461bdc5989af48e7e
Mikk <mikk@you.die.ifyou.work>
2025-06-16 02:12:31 +0300

    fix nocl

--- nsec3_nocl.go
+++ nsec3_nocl.go
@@ -4,7 +4,7 @@ package main
 
 import "iter"
 
-const hasOpenCL = true
+const hasOpenCL = false
 
 func nsec3HashOpenCLIter(zone, salt []byte, iterations int) iter.Seq[hashEntry] {
 	panic("unreachable")
